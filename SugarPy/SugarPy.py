e733482e2d26ae778d178597068227b0addb4ddf
Stephen Edwards <stephen.edwards@foostix.com>
2020-02-09 16:32:26 +0100

    some updates

--- SugarPy/SugarPy.py
+++ SugarPy/SugarPy.py
@@ -1,17 +1,40 @@
 import tkinter as tk
+import threading
 import urllib.request, json 
+import sched, time
+   
 
-with urllib.request.urlopen("http://sugarmate.io/api/v1/83y9kt/latest.json") as url:
-    data = json.loads(url.read().decode())
-    print(data)
+class App(threading.Thread):
+    glyco_text = "waiting for value"
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.start()
 
-root = tk.Tk()
-root.attributes('-fullscreen', True)
-glyco_text = str(data["value"])+"mg/dl"
+    def callback(self):
+        self.root.quit()
 
-label = tk.Label(root, text=glyco_text, font=("Helvetica", 48)) # Create a text label
+    def setGlycoText(self, valueText):
+        print("Updating value to "+valueText)
+        self.glyco_text = valueText
+        self.label['text'] = valueText+" mg/dl"
 
-label.pack(padx=20, pady=20, expand=1) # Pack it into the window
+    def run(self):
+        self.root = tk.Tk()
+        #root.attributes('-fullscreen', True)
+        self.label = tk.Label(self.root, text=self.glyco_text, font=("Helvetica", 48)) # Create a text label
+        self.label.pack(padx=20, pady=20, expand=1) # Pack it into the window
+        self.root.mainloop()
 
+app = App()
 
-root.mainloop()
+def getGlycoApi(s):
+    with urllib.request.urlopen("http://sugarmate.io/api/v1/83y9kt/latest.json") as url:
+        data = json.loads(url.read().decode())    
+        app.setGlycoText(str(data["value"]))
+        s.enter(5, 1, getGlycoApi, (s,))
+
+s = sched.scheduler(time.time, time.sleep)
+s.enter(5, 1, getGlycoApi, (s,))
+s.run()
+    
+
